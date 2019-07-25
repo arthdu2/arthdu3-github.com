bec9bc5fa80b19103e064e99e7fc6feecd69b980
arthdu2 <arthdu2@gmail.com>
2019-07-25 16:30:24 -0300

    cache sw

---
+++ sw.js
@@ -0,0 +1,14 @@
+self.addEventListener('install', function(event) {
+    event.waitUntil(
+      caches.open(cacheName).then(function(cache) {
+        return cache.addAll(
+          [
+        
+            '/mystyle.css',
+            '/script.js',
+            '/index.html'
+          ]
+        );
+      })
+    );
+  });
